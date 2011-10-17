02b0d6f6864b80d8ea4825c71a38eca51f51436f
Jestan Nirojan <jestannirojan@gmail.com>
2011-10-17 17:59:48 +0530

    downgraded joda-time library version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject org.clojars.hms/clj-time "0.3.2"
   :description "A date and time library for Clojure, wrapping Joda Time."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [joda-time "2.0"]]
+                 [joda-time "1.6.2"]]
 
   :repositories {"releases" "http://192.168.0.7:8080/archiva/repository/internal/"
                  "snapshots" "http://192.168.0.7:8080/archiva/repository/snapshots/"})
