f01f43846bdfaf6976303392d1ecd05b874e93fb
Jestan Nirojan <jestannirojan@gmail.com>
2011-10-17 16:06:45 +0530

    only included clojure 1.3.0 deps

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
-(defproject clj-time "0.3.2-SNAPSHOT"
+(defproject clj-time "0.3.2"
   :description "A date and time library for Clojure, wrapping Joda Time."
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [joda-time "2.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [joda-time "2.0"]]
+
+  :repositories {"releases" "http://192.168.0.7:8080/archiva/repository/internal/"
+                 "snapshots" "http://192.168.0.7:8080/archiva/repository/snapshots/"})
